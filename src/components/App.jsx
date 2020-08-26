20ead9980c4f674a0d76f844cc1094d0cee902b0
= <=>
2020-08-25 21:24:45 -0500

    [ADD] header section added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,29 @@
 import React from "react";
+import { Switch, Route, BrowserRouter, Link } from "react-router-dom";
+import headerBackground from "../assets/images/Banner.jpg";
 import Layout from "./Layout";
+import Header from "./header/Header";
+import Filter from "../components/filter/Filter";
 
-function App() {
-  return (
-    <React.Fragment>
-      <Layout></Layout>
-    </React.Fragment>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route
+            exact
+            path="/"
+            render={(props) => (
+              <Header {...props} bgImage={headerBackground} text={"Hola desde App.jsx"}>
+                <Filter />
+              </Header>
+            )}
+          />
+        </Switch>
+        <Layout></Layout>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
