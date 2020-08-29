2a49779a6d2cd25de3e30e6abaf311198980310e
= <=>
2020-08-29 13:33:53 -0500

    [FIX] footer fixed and placed in the right component

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,9 +1,15 @@
 import React from "react";
+import Footer from "./footer/Footer";
 
 const Layout = (props) => {
   const { children } = props;
 
-  return <React.Fragment>{children}</React.Fragment>;
+  return (
+    <React.Fragment>
+      {children}
+      <Footer />
+    </React.Fragment>
+  );
 };
 
 export default Layout;
