b99719a3e7d6408a68b529f0f6c933125bec2059
Fredy Frasser <fredyfrasser@hotmail.com>
2020-09-12 10:19:19 -0500

    responsivo

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,10 +1,10 @@
-import React from 'react';
-import { NavLink, Link } from 'react-router-dom';
-import '../../assets/styles/components/header/Header.scss';
-import focusLogo from '../../assets/images/logo-focus.svg';
-import githubLogo from '../../assets/images/github.svg';
-import linkedinLogo from '../../assets/images/linkedin.svg';
-import facebookLogo from '../../assets/images/facebook.svg';
+import React from "react";
+import { NavLink, Link } from "react-router-dom";
+import "../../assets/styles/components/header/Header.scss";
+import focusLogo from "../../assets/images/logo-focus.svg";
+import githubLogo from "../../assets/images/github.svg";
+import linkedinLogo from "../../assets/images/linkedin.svg";
+import facebookLogo from "../../assets/images/facebook.svg";
 
 const Header = (props) => {
   const { children, bgImage } = props;
@@ -12,54 +12,54 @@ const Header = (props) => {
     backgroundImage: `url(${bgImage})`,
   };
   return (
-    <header className='header' style={headerBackground}>
-      <i className='icon-menu burger-button' id='burger-menu' />
-      <nav className='menu'>
-        <figure className='logo'>
-          <NavLink to='/'>
-            <img src={focusLogo} height='30' alt='Logo Focus' />
+    <header className="header" style={headerBackground}>
+      {/* <i className="icon-menu burger-button" id="burger-menu" /> */}
+      <nav className="menu">
+        <figure className="logo">
+          <NavLink to="/">
+            <img src={focusLogo} height="30" alt="Logo Focus" />
           </NavLink>
         </figure>
-        <ul className='menu-items'>
+        <ul className="menu-items">
           <li>
-            <NavLink className='link' to='#nosotros'>
+            <NavLink className="link" to="#nosotros">
               Nosotros
             </NavLink>
           </li>
           <li>
-            <NavLink className='link' to='#vip'>
+            <NavLink className="link" to="#vip">
               Focus VIP
             </NavLink>
           </li>
           <li>
-            <NavLink className='link' to='#empresas'>
+            <NavLink className="link" to="#empresas">
               Empresas
             </NavLink>
           </li>
           <li>
-            <NavLink className='link' to='#contacto'>
+            <NavLink className="link" to="#contacto">
               Contacto
             </NavLink>
           </li>
         </ul>
         <ul>
-          <li className='social-icon'>
-            <Link to='/'>
-              <img className='' src={githubLogo} width='20' alt='GitHub' />
+          <li className="social-icon">
+            <Link to="/">
+              <img className="" src={githubLogo} width="20" alt="GitHub" />
             </Link>
-            <p className='social-text'>Github</p>
+            <p className="social-text">Github</p>
           </li>
-          <li className='social-icon'>
-            <Link to='/'>
-              <img className='' src={linkedinLogo} width='20' alt='Linkedin' />
+          <li className="social-icon">
+            <Link to="/">
+              <img className="" src={linkedinLogo} width="20" alt="Linkedin" />
             </Link>
-            <p className='social-text'>Linkedin</p>
+            <p className="social-text">Linkedin</p>
           </li>
-          <li className='social-icon'>
-            <Link to='/'>
-              <img className='' src={facebookLogo} width='20' alt='Facebook' />
+          <li className="social-icon">
+            <Link to="/">
+              <img className="" src={facebookLogo} width="20" alt="Facebook" />
             </Link>
-            <p className='social-text'>Facebook</p>
+            <p className="social-text">Facebook</p>
           </li>
         </ul>
       </nav>
